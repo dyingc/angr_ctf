be58865513a4efc7aac02c5afc783fa8fe8ef1c9
ying.dong <ying.dong@oracle.com>
2025-07-20 12:29:20 -0700

    Improve  cross-platform compilation

--- 00_angr_find/generate.py
+++ 00_angr_find/generate.py
@@ -3,7 +3,7 @@
 # 导入所需的库：
 # tempfile: 用于创建临时文件。
 # jinja2: 一个 Python 的模板引擎，用于生成动态文本内容。
-import sys, random, os, tempfile, jinja2
+import sys, random, os, tempfile, jinja2, platform
 
 def generate(argv):
   # 检查命令行参数的数量。脚本需要两个参数：种子和输出文件名。
@@ -47,15 +47,29 @@ def generate(argv):
     # 将文件指针移到文件开头，以便后续读取（虽然这里没有读取，但这是使用 temp.name 的常见做法，下面的代码会用到它）。
     temp.seek(0)
 
+    # 根据架构选择合适的编译参数
+    arch = platform.machine()
+    if arch.startswith("x86"):
+      # 适用于 x86_64 架构的编译命令
+      # -fno-pie: 禁用位置无关代码生成（Piece）。
+      # -no-pie: 禁用位置无关可执行文件生成。(radare2 / rizin 的`pdf`命令显示的是rebased地址)
+      # -fcf-protection=none: 禁用控制流保护（如 CET (Control-flow Enforcement Technology)）
+      # -fno-stack-protector: 禁用栈保护
+      # -O0: 无优化，保持源码与汇编的完美对应关系
+      # -g: 生成调试符号信息（DWARF格式），支持 GDB 源码级调试
+      # -m32: 生成 32 位可执行文件。
+      compile_cmd = 'gcc -fno-pie -no-pie -fcf-protection=none -fno-stack-protector -m32 -O0 -g -o ' + output_file + ' ' + temp.name
+    elif arch == 'arm64':
+      # 适用于 arm64 架构的编译命令 (Apple Silicon)
+      compile_cmd = f'gcc -fno-stack-protector -O0 -g -o {output_file + "_arm"} {temp.name}'
+    else:
+      # 其他架构的默认编译命令
+      compile_cmd = f'gcc -fno-stack-protector -O0 -g -o {output_file + "_other"} {temp.name}'
+
     # 使用 gcc 编译 C 代码。
-    # 'gcc -fno-pie -no-pie -fcf-protection=none -m32 -o ' + output_file + ' ' + temp.name
-    # -fno-pie: 禁用位置无关代码生成（Piece）。
-    # -no-pie: 禁用位置无关可执行文件生成。(radare2的`pdf`命令显示的是rebased地址)
-    # -fcf-protection=none: 禁用控制流保护（如 CFI）。
-    # -m32: 生成 32 位可执行文件。
     # -o output_file: 指定输出的可执行文件名。
     # temp.name: 指定要编译的源文件（临时 C 文件）。
-    os.system('gcc -fno-pie -no-pie -fcf-protection=none -m32 -o ' + output_file + ' ' + temp.name)
+    os.system(compile_cmd)
 
 if __name__ == '__main__':
     generate(sys.argv)
